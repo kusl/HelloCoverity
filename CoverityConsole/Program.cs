b9007a5e63768c37e061a75bac7a7e624286efd0
kushal <kushaldeveloper@gmail.com>
2021-04-26 23:46:33 +0000

    init

---
+++ CoverityConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoverityConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
